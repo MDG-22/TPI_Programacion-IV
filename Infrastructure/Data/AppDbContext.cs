1f794487bab7299569be3b2ab07a1d4a07b2ce94
MDG-22 <116525431+MDG-22@users.noreply.github.com>
2025-10-30 11:46:32 -0300

    basic db install, no models

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // entities tables
+        public DbSet<User> Users { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+
+
+
+    }
+}
